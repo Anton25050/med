4857fc52d1fe4e9370b03721470d0628b140ba5d
Student <student@kol.tech.lan>
2024-02-29 18:52:10 +0300

    начал делать главную старницу

--- views/site/index.php
+++ views/site/index.php
@@ -2,52 +2,64 @@
 
 /** @var yii\web\View $this */
 
-$this->title = 'My Yii Application';
+$this->title = 'МедПлюс';
 ?>
-<div class="site-index">
-
-    <div class="jumbotron text-center bg-transparent mt-5 mb-5">
-        <h1 class="display-4">Congratulations!</h1>
-
-        <p class="lead">You have successfully created your Yii-powered application.</p>
-
-        <p><a class="btn btn-lg btn-success" href="https://www.yiiframework.com">Get started with Yii</a></p>
-    </div>
-
-    <div class="body-content">
-
-        <div class="row">
-            <div class="col-lg-4 mb-3">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-outline-secondary" href="https://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
-            </div>
-            <div class="col-lg-4 mb-3">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-outline-secondary" href="https://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-outline-secondary" href="https://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
-            </div>
-        </div>
-
-    </div>
+<style>
+    .img {
+  display: inline-block;
+  max-width: 18%;
+  height: auto;
+  position: relative;
+  left: 900px;
+  top: -120px;
+}
+.img2 {
+  display: inline-block;
+  max-width: 80%;
+  height: auto;
+  position: relative;
+  left: 50px;
+  top: -30px;
+}
+    .pos {
+  display: inline-block;
+  position: relative;
+  left: 90px;
+  top: 20px;  
+    }
+</style>
+<div class="container pos">
+  <span class="fs-1 fw-bolder"><span class="text-primary">Мед</span><span class="text-black">Плюс</span></span>
 </div>
+    <div class='container pos'>
+        <div class='fs-6 text-muted text-wrap' style="width: 25rem;">Медцентр, клиника, Психиатрическая клиника, Медицинская реабилитация</div>
+        <div class='img'><img src="web\images\bol.png" class='img-fluid' alt="..."></div>
+    </div>
+        <div class='container pos'>
+        <div class="img2"><img src="web\images\pal.jpg" class='img-thumbnail' alt="..."></div>  
+        </div>
+            <div class="container pos">
+            <div class='fs-3 fw-semibold'>О нас</div>
+    <div class="row align-items-start">
+        <div class="col">
+        <div class="text-muted text-wrap" style="width: 13rem;">
+        Парковка для инвалидов
+        Тест на коронавирус
+        Парковка    
+        </div>
+        </div>
+        <div class="col">
+        <div class="text-muted text-wrap" style="width: 13rem;">
+        Оплата картой
+        Вызов врача на дом
+        Для детей
+        </div>
+        </div>
+        <div class="col">
+        <div class="text-muted text-wrap" style="width: 13rem;">
+        Анонимное обследование
+        Массаж
+        </div>
+        </div>
+    </div>
+    </div>
