497308180a064bc64d66454ad6b014bba65517cb
Anton25050 <Antone08487@gmail.com>
2024-03-03 22:27:38 +0300

    добавил раздел преимущества

--- views/site/index.php
+++ views/site/index.php
@@ -21,11 +21,44 @@ $this->title = 'МедПлюс';
   left: 50px;
   top: -30px;
 }
+.img3 {
+  display: inline-block;
+  max-width: 5%;
+  height: auto;
+  position: relative;
+  left: -320px;
+  top: -5px;
+}
+.img4 {
+display: inline-block;
+  max-width: 6%;
+  height: auto;
+  position: relative;
+  left: -390px;
+  top: 47px; 
+}
+.img5 {
+display: inline-block;
+  max-width: 5%;
+  height: auto;
+  position: relative;
+  left: -462px;
+  top: 87px; 
+}
     .pos {
   display: inline-block;
   position: relative;
   left: 90px;
   top: 20px;  
+    }
+
+    .tech {
+    line-height: 2.8;
+    }
+    .mem {
+    background-size: auto;
+    background: #C0C0C0;
+    border-radius: 5px;
     }
 </style>
 <div class="container pos">
@@ -41,25 +74,41 @@ $this->title = 'МедПлюс';
             <div class="container pos">
             <div class='fs-3 fw-semibold'>О нас</div>
     <div class="row align-items-start">
+
         <div class="col">
-        <div class="text-muted text-wrap" style="width: 13rem;">
+        <div class="text-muted text-wrap tech" style="width: 13rem;">
         Парковка для инвалидов
         Тест на коронавирус
         Парковка    
         </div>
         </div>
-        <div class="col">
+        <div class="img3"><img src="web\images\invalid.png" class='img-fluid' alt="...">
+    </div>
+        <div class="img4"><img src="web\images\test.png" class='img-fluid' alt="...">
+    </div>
+        <div class="img5"><img src="web\images\park.png" class='img-fluid' alt="...">
+    </div> 
+        <div class="col-5 tech">
         <div class="text-muted text-wrap" style="width: 13rem;">
         Оплата картой
         Вызов врача на дом
         Для детей
         </div>
         </div>
-        <div class="col">
+        <div class="col tech">
         <div class="text-muted text-wrap" style="width: 13rem;">
         Анонимное обследование
         Массаж
         </div>
         </div>
+    </div>
+    </div>
+        <div class='container pos'>
+        <div class='fs-3 fw-semibold'>Наши преимущества</div>
+        <div class="row align-items-center">
+            <p class= 'fs-6 text-wrap bg-light' style='width:60rem; border-radius: 8px;'><span class='fw-bold'>Широкий спектр услуг.</span>Медицинский центр работает в формате универсальной семейной клиники. В сферу нашей деятельности входят все виды медицинской помощи: диагностика, лечение, восстановление и профилактика.</p>
+            <p class= 'fs-6 text-wrap bg-light' style='width:60rem; border-radius: 8px;'><span class='fw-bold'>Профессионализм и большой опыт работы.</span>Прием пациентов ведут квалифицированные врачи, за плечами которых длительная практика и широкие знания в своей сфере. В штате медицинских центров работают врачи высшей категории, 40 кандидатов медицинских наук, 4 доктора медицинских наук и 2 профессора.</p>
+            <p class= 'fs-6 text-wrap bg-light' style='width:60rem; border-radius: 8px;'><span class='fw-bold'>Современное оборудование.</span>Благодаря хорошему оснащению клиники мы предлагаем пациентам разные виды исследований: КТ, МРТ, рентген, УЗИ, эндоскопия, функциональная диагностика, лабораторные анализы. В кабинетах специалистов есть необходимое оборудование, которое позволяет поставить диагноз уже во время приема.</p>
+            <p class= 'fs-6 text-wrap bg-light' style='width:60rem; border-radius: 8px;'><span class='fw-bold'>Высокий уровень обслуживания.</span>Мы стараемся сделать посещение медицинского центра комфортным для каждого пациента. Филиалы работают по удобному графику, у нас нет очередей. Персонал клиники — вежливые и деликатные сотрудники.</p>
     </div>
     </div>
